dddd30cbafae4b540a3231b5426a6c854e7eec10
Timothy Ogilvy <tim@Timothys-MacBook-Air.local>
2017-03-15 17:21:40 +1100

    has tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,54 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Display All Tasks
+ */
+Route::get('/tasks', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/tasks');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/tasks');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
